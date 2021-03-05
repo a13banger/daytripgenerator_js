aeb2cd5012e647a58a67812af3f9afc592e30e12
Dustin <dustin@Oz-Makina.attlocal.net>
2021-03-05 14:59:39 -0600

    Duplicated the method used in destinations function in the three remaining randomizers.

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -2,7 +2,6 @@
 // console.log("File Connected")
 
 let destinations = ['Florida', 'Washington', 'Texas', 'Kentucky', 'Alaska'];
-
 function destinationPicker() {
     let destIndex = Math.floor(Math.random() * destinations.length);
     return destinations[destIndex];
@@ -12,21 +11,26 @@ console.log(result);
 
 
 let restaurants = ['McDonalds', 'Burger King', 'Subway', 'Raising Canes Chicken', 'Andolinis Pizza'];
-
 function restaurantPicker() {
-
+    let restPicker = Math.floor(Math.random() * restaurants.length);
+    return restaurants[restPicker];
 }
+let restResults = restaurantPicker();
+console.log(restResults);
 
 
 let transportation = ['Self Drive', 'Uber', 'Book Flight', 'StarShip'];
-
 function transportationPicker() {
-
+    let transIndex = Math.floor(Math.random() * transportation.length);
+    return transportation[transIndex];
 }
-
+let transResults = transportationPicker();
+console.log(transResults);
 
 let entertainment = ['Movie Tickets', 'Live Band', 'Sporting Event', 'Murder Mystery Dinner'];
-
 function entertainmentPicker() {
-
+    let enterIndex = Math.floor(Math.random() * entertainment.length);
+    return entertainment[enterIndex];
 }
+let enterResults = entertainmentPicker();
+console.log(enterResults);
