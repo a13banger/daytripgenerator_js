7ad66ef0c4ca1694b6e4583218ca878680a3d9ab
Dustin <dustin@Oz-Makina.attlocal.net>
2021-03-05 14:47:06 -0600

    Created and tested first randomizer function for destinations.

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -1,15 +1,32 @@
+"use strict";
 // console.log("File Connected")
 
 let destinations = ['Florida', 'Washington', 'Texas', 'Kentucky', 'Alaska'];
 
+function destinationPicker() {
+    let destIndex = Math.floor(Math.random() * destinations.length);
+    return destinations[destIndex];
+}
+let result = destinationPicker();
+console.log(result);
+
 
 let restaurants = ['McDonalds', 'Burger King', 'Subway', 'Raising Canes Chicken', 'Andolinis Pizza'];
+
+function restaurantPicker() {
+
+}
 
 
 let transportation = ['Self Drive', 'Uber', 'Book Flight', 'StarShip'];
 
+function transportationPicker() {
+
+}
+
 
 let entertainment = ['Movie Tickets', 'Live Band', 'Sporting Event', 'Murder Mystery Dinner'];
 
+function entertainmentPicker() {
 
-
+}
