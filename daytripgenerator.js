26acc12db7b1a52fab77105e2f90fc8e44469c9a
Dustin <dustin@Oz-Makina.attlocal.net>
2021-03-05 14:10:26 -0600

    Established the arrays needed to contain user choices for the trip. Declared a variable for each of the four options

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -1,1 +1,15 @@
-console.log("File Connected")
+// console.log("File Connected")
+
+let destinations = ['Florida', 'Washington', 'Texas', 'Kentucky', 'Alaska'];
+
+
+let restaurants = ['McDonalds', 'Burger King', 'Subway', 'Raising Canes Chicken', 'Andolinis Pizza'];
+
+
+let transportation = ['Self Drive', 'Uber', 'Book Flight', 'StarShip'];
+
+
+let entertainment = ['Movie Tickets', 'Live Band', 'Sporting Event', 'Murder Mystery Dinner'];
+
+
+
